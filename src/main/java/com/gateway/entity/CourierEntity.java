31fcba6759290051cd8d9b70dd77714166acf0b0
Verg92 <maurovann@gmail.com>
2021-06-29 10:51:36 +0200

    Create CourierEntity.java

---
+++ src/main/java/com/gateway/entity/CourierEntity.java
@@ -0,0 +1,22 @@
+package com.gateway.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourierEntity {
+
+	private String service;
+	private String url;
+	private String serviceUsername;
+	private String servicePassword;
+	private String accountUsername;
+	private String accountPassword;
+	private String token;
+
+}

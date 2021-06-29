3e76d7d5fbd51dd7e02cfc181a1d7bcd31950383
Verg92 <maurovann@gmail.com>
2021-06-29 14:47:42 +0200

    add specification

--- src/main/java/com/gateway/repository/CourierRepository.java
+++ src/main/java/com/gateway/repository/CourierRepository.java
@@ -1,5 +1,7 @@
 package com.gateway.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,7 @@ import com.gateway.entity.CourierEntity;
 
 @Repository
 public interface CourierRepository extends JpaRepository<CourierEntity, Long>, JpaSpecificationExecutor<CourierEntity> {
+
+	List<CourierEntity> findAllByIdLike(Long id);
 
 }
--- src/main/java/com/gateway/specification/CourierSpecification.java
+++ src/main/java/com/gateway/specification/CourierSpecification.java
@@ -1,5 +1,15 @@
 package com.gateway.specification;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.gateway.entity.CourierEntity;
+import com.gateway.entity.CourierEntity_;
+
 import lombok.Data;
 
 @Data
@@ -13,5 +23,17 @@ public class CourierSpecification {
 	private String accountUsername;
 	private String accountPassword;
 	private String token;
+
+	public static Specification<CourierEntity> customerHasBirthday() {
+		return new Specification<CourierEntity>() {
+
+			private static final long serialVersionUID = 5003585443426119934L;
+
+			public Predicate toPredicate(Root<CourierEntity> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				return cb.equal(root.get(CourierEntity_.url), null);
+			}
+
+		};
+	}
 
 }
